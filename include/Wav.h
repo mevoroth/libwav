5d72aca576faca6fd2e398aa5bd2bca99a034712
Daniel Quach <daniel@quach.info>
2025-03-23 16:29:47 +0000

    Marker constant helper

--- include/Wav.h
+++ include/Wav.h
@@ -14,11 +14,20 @@ namespace Wav
 		 * @param[in] Extern buffer
 		 * @param[in] Size of extern buffer
 		 */
-		Marker(u32 InWord);
+		Marker(u32 InWord = 0u);
+
+		bool operator==(_In_ const Marker& InMarker) const;
 
 		char Bytes[WordBytes];
 		u32 Word;
 	};
+
+	extern const Marker MarkerRIFF;
+	extern const Marker MarkerWAVE;
+	extern const Marker Markerfmt_;
+	extern const Marker MarkerLIST;
+	extern const Marker Markerdata;
+	extern const Marker MarkerINFO;
 
 	struct Header
 	{
@@ -26,10 +35,10 @@ namespace Wav
 		static constexpr u32 DefaultSamplesPerSecond	= 44100u;
 		static constexpr u32 DefaultBitsPerSample		= 16u;
 
-		Marker RIFFMarker		= 'RIFF';
+		Marker RIFFMarker		= MarkerRIFF;
 		u32 FileSize			= 0u;
-		Marker WAVEMarker		= 'WAVE';
-		Marker FMTMarker		= 'FMT ';
+		Marker WAVEMarker		= MarkerWAVE;
+		Marker FMTMarker		= Markerfmt_;
 		u32 SubChunkSize		= 0u;
 		u16 AudioFormat			= 1u;
 		u16 ChannelsCount		= DefaultChannelsCount;
@@ -41,7 +50,7 @@ namespace Wav
 
 	struct Data
 	{
-		Marker DATAMarker		= 'data';
+		Marker DATAMarker		= Markerdata;
 		u32 DataSize			= 0u;
 	};
 }
--- src/Wav.cpp
+++ src/Wav.cpp
@@ -2,8 +2,24 @@
 
 namespace Wav
 {
+	const Marker MarkerRIFF = 'RIFF';
+	const Marker MarkerWAVE = 'WAVE';
+	const Marker Markerfmt_ = 'fmt ';
+	const Marker MarkerLIST = 'LIST';
+	const Marker Markerdata = 'data';
+	const Marker MarkerINFO = 'INFO';
+
 	Marker::Marker(u32 InWord)
 		: Word(InWord)
 	{
+		Word = ((Word & 0x000000FF) << 24)
+			 | ((Word & 0x0000FF00) << 8)
+			 | ((Word & 0x00FF0000) >> 8)
+			 | ((Word & 0xFF000000) >> 24);
+	}
+
+	bool Marker::operator==(_In_ const Marker& InMarker) const
+	{
+		return Word == InMarker.Word;
 	}
 }
